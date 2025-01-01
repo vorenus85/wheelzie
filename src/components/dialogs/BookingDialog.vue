1066926fd6e5daf5e72a0e28dcbce8dc4e30e2b6
Janos Perge <janos.perge@gmail.com>
2025-01-01 21:55:51 +0100

    add unit testing coverage

---
+++ src/components/dialogs/BookingDialog.vue
@@ -0,0 +1,40 @@
+<template>
+  <Dialog
+    :header="headerTitle"
+    v-model:visible="visible"
+    :style="{ width: '650px' }"
+    :modal="true"
+    @update:visible="closeDialog"
+  >
+    <form @submit.prevent="validateAndSave" ref="form"></form>
+  </Dialog>
+</template>
+<script setup>
+import Dialog from 'primevue/dialog'
+import { ref, watch } from 'vue'
+const emit = defineEmits(['save', 'hide'])
+const visible = ref(false)
+const props = defineProps({
+  booking: { type: Object, default: () => ({}) },
+  showDialog: Boolean
+})
+const headerTitle = ref('Add new booking')
+
+watch(
+  () => props.showDialog,
+  newValue => {
+    headerTitle.value = props.booking.id ? 'Edit booking' : 'Add new booking'
+    // currentClient.value = { ...props.client }
+    visible.value = newValue
+  }
+)
+
+// Validation and save function
+const validateAndSave = async () => {}
+
+// Close dialog
+const closeDialog = () => {
+  // resetForm()
+  emit('hide')
+}
+</script>
--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,25 @@
 import { fileURLToPath } from 'node:url'
-import { mergeConfig, defineConfig, configDefaults } from 'vitest/config'
+import { configDefaults, defineConfig, mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      globals: true,
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/**'],
+      coverage: {
+        provider: 'istanbul',
+        exclude: ['src/App.vue', 'src/main.js', '.eslintrc.cjs  '],
+        thresholds: {
+          // Thresholds for basic level
+          statements: 50,
+          functions: 60,
+          branches: 40,
+          lines: 50
+        }
+      },
+      exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url))
     }
   })
